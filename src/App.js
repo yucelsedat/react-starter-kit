1f4ff2c4caa64c19382fe5b1c21343705c4f270f
yucelsedat <yucelsedatt@gmail.com>
2024-02-05 19:54:05 +0300

    deneme

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import logo from './logo.svg'
 function App() {
   return (
     <div className="App">
-      <img src="logo192.png" alt="logo1"></img>
-      <img src={logo} alt="logo1"></img>
-    </div>
+    deneme
+    </div> 
   );
 }
 
